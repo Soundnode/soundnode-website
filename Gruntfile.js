8d8d9450808d2ebbea35f3918cff874f1adb9d7f
MLancaster <mlancaster@mlancaster-m1.roundarch.com>
2015-04-16 17:34:36 -0500

    add imagemin grunt task and express file compression

--- Gruntfile.js
+++ Gruntfile.js
@@ -24,16 +24,29 @@ module.exports = function(grunt) {
 			      	'public/stylesheets/app.css'
 			      ]
 			    }
-			  }
+			}
+		},
+		imagemin: {
+		    dist: {
+				files: [{
+					expand: true,
+					cwd: 'public/images',
+					src: ['**/*.{png,jpg,gif}'],
+					dest: 'public/images/'
+				}]
+			}
 		}
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-imagemin');
+
 	grunt.registerTask('build', [
 		'uglify', 
-		'cssmin'
+		'cssmin',
+		'imagemin'
 	]);
 	grunt.event.on('watch', function(action, filepath) {
 	  grunt.log.writeln(filepath + ' has ' + action);
--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 var express = require('express')
     , morgan = require('morgan')
+    , compression = require('compression')
     , bodyParser = require('body-parser')
     , methodOverride = require('method-override')
     , app = express()
-    , port = process.env.PORT || 3000
+    , port = process.env.PORT || 4000
     , router = express.Router();
 
 app.use(express.static(__dirname + '/views'));
 app.use(express.static(__dirname + '/public'));
+app.use(compression());
 app.use(morgan('dev'));
 app.use(bodyParser());
 app.use(methodOverride());
