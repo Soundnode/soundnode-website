25e9ec2e7cede7c3eff595ad2823514bac897ad7
wb9688 <wbertrums@gmail.com>
2016-04-28 09:24:49 +0200

    Added changes you suggested

--- assets/js/fixOffset.js
+++
@@ -1,8 +0,0 @@
-function fixOffset() {
-  setTimeout(function() {
-    scrollBy(0, -51)
-  }, 5)
-}
-
-document.getElementsByClassName('navbar-brand')[0].addEventListener('click', fixOffset)
-document.getElementsByClassName('featuredon')[0].addEventListener('click', fixOffset)
--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 var express = require('express'),
+    morgan = require('morgan'),
     shrinkRay = require('shrink-ray'),
     app = express(),
     port = process.env.PORT || 4000
 
 app.use(shrinkRay())
-app.use(express.static(__dirname + '/static'))
+app.use(morgan('dev'))
+app.use(express.static(__dirname + '/public'))
 
 app.listen(port, function () {
   console.log('App listening on port ' + port + '...')
