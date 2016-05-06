5faa800ed27d82b98ef1c84e31d88da1f0c6f491
wb9688 <wbertrums@gmail.com>
2016-05-06 09:50:17 +0200

    Made navbar black and changed button styles

--- server.js
+++ server.js
@@ -8,6 +8,6 @@ app.use(shrinkRay())
 app.use(morgan('dev'))
 app.use(express.static(__dirname + '/public'))
 
-app.listen(port, function () {
+app.listen(port, function() {
   console.log('App listening on port ' + port + '...')
 })
